88f20d432357a04990c3961e456e8474c18db6bc
Yurii Tern <ternxstyle@gmail.com>
2021-12-26 05:50:12 +0200

    05-aim-game-sources_v.3.0-added_new_colors

--- 05-aim-game-sources/app.js
+++ 05-aim-game-sources/app.js
@@ -3,6 +3,8 @@ const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
+const colors = ['#cd5c5c', '#ffc0cb', '#ffa07a', '#ffd700', '#e6r6fa', '#fff8dc', '##adff2f', '#00ffff', '#dcdcdc']
+
 let time = 0
 let score = 0
 
@@ -51,7 +53,7 @@ function setTime(value) {
 
 function finishGame() {
   timeEl.parentNode.classList.add('hide')
-  board.innerHTML = `<h1>Ваш счёт: <span class = "primary">${score}</span></h1>`  
+  board.innerHTML = `<h1>Ваш счёт: <span class = "primary">${score}</span></h1>`
 }
 
 function createRandomCircle() {
@@ -60,13 +62,15 @@ function createRandomCircle() {
   const {width, height} = board.getBoundingClientRect()
   const x = getRandomNumber(0, width - size)
   const y = getRandomNumber(0, height - size)
-  
+
+  setColor(circle)
+
   circle.classList.add('circle')
   circle.style.width = `${size}px`
   circle.style.height = `${size}px`
   circle.style.top = `${y}px`
-  circle.style.left = `${x}px` 
-  
+  circle.style.left = `${x}px`
+
   board.append(circle)
 }
 
@@ -74,3 +78,15 @@ function getRandomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min)
 }
 
+// Randomcolors
+
+function setColor(element) {
+  const color = getRandomColor()
+  element.style.background = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
+}
